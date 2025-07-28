c93497b1a78d40e093f685d51811cd98b5b7fc65
sultanpr <aspiresawon@gmail.com>
2025-07-28 09:51:27 +0600

    preOrder tree print

--- PreOrderTree.cpp
+++ PreOrderTree.cpp
@@ -16,10 +16,31 @@ class Node{
     }
 }; 
 
+void preOrder(Node* root)
+{
+    if(root == NULL)
+        return; 
+    cout << root->val << " ";
+    preOrder(root->left); 
+    preOrder(root->right); 
+}
 
 int main()
 {
-    
+    Node* root = new Node(10);
+    Node* a = new Node(21); 
+    Node* b = new Node(43); 
+    Node* c = new Node(27); 
+    Node* d = new Node(87); 
+    Node* e = new Node(56); 
+
+    root->left = a; 
+    root->right = b;
+    a->left = c; 
+    a->right = d; 
+    b->left = e; 
+
+    preOrder(root); 
 
     return 0; 
 }
